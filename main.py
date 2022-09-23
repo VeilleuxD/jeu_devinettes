b32a69579740191ba4eb84b5901a90a96e93a2de
veilleuxd <veilleuxd@educ.college-montreal.qc.ca>
2022-09-23 16:00:02 -0400

    Respect du PEP8

--- main.py
+++ main.py
@@ -1,45 +1,43 @@
-#David A. Veilleux
-#405
-#15/9/22  D/M/Y
+"""
+nom: David A. Veilleux
+Groupe: 405
+Description: Un petit jeu ou le joueur doit deviner le nombre choisi aléatoirement entre 1 et 100.
 
+"""
 
 import random
 
 
-jouer=True
+jouer = True
 
 
 while jouer:
-
-
     borne_min = 0
     borne_max = 100
 
     number = random.randint(borne_min, borne_max)
     nbr_essai = 0
 
-#plus tard je donnera la possibilité de choisir les bornes
+# plus tard je donnera la possibilité de choisir les bornes
     print("J'ai choisi un nombre entre " + str(borne_min) + " et " + str(borne_max) + ". À vous de le trouver!")
 
-#Merci Cabana
-    while  (answer:=int(input("\nInsérez votre réponse ici! \n-->"))) != number:
+# Merci Cabana
+    while (answer := int(input("\nInsérez votre réponse ici! \n-->"))) != number:
 
         if answer < number:
-            print ("La réponse est plus grande")
+            print("La réponse est plus grande")
 
         elif answer > number:
-            print ("la réponse est plus petite!")
+            print("la réponse est plus petite!")
 
         nbr_essai = nbr_essai + 1
 
     print("Voilà la bonne réponse! C'était bien " + str(number) + "!")
-    print ("Vous avez réussi en " + str(nbr_essai) + " essai(s)!")
-
-
+    print("Vous avez réussi en " + str(nbr_essai) + " essai(s)!")
     leave = input("\n\nVoulez-vous jouer encore? y/n\n-->")
-    if leave == ("y"):
+    if leave == "y":
         jouer = True
     else:
-        print ("Merci d'avoir joué!\n:)")
+        print("Merci d'avoir joué!\n:)")
 
         jouer = False
